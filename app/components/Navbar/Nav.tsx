bd749d2e856f2c27f32eac2227ce1c58d76ecdbc
IcodeAlpha <alphacode4@outlook.com>
2024-10-11 12:30:00 +0300

    mobilenav

--- app/components/Navbar/Nav.tsx
+++ app/components/Navbar/Nav.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 import Link from 'next/link'
 import { BiMenu } from 'react-icons/bi'
 
-const Nav = () => {
+interface Props {
+    openNav:()=>void,
+}
+
+const Nav = ({openNav}: Props) => {
   return (
     <div className='h-[12vh] shadow-md'>
         <div className='flex items-center justify-between h-[100%] w-[90%] mx-auto'>
@@ -32,7 +36,7 @@ const Nav = () => {
             <div className='flex items-center space-x-3'>
                 <button className='px-6 py-2 sm:px-8 sm:py-2.5 transition-all duration-300 bg-blue-600 hover:bg-blue-800 text-white font-semibold'>Join Now</button>
                 {/* Menu */}
-                <BiMenu className='w-[2rem] h-[2rem] text-red-800 lg:hidden' />
+                <BiMenu onClick={openNav} className='w-[2rem] h-[2rem] text-red-800 lg:hidden' />
             </div>
         </div>
     </div>
