647ca1b51a0117d916a70d5fe118e0731a52536c
IcodeAlpha <alphacode4@outlook.com>
2024-11-23 19:48:01 +0300

    new latest news

--- app/components/LatestNews/LatestNews.tsx
+++ app/components/LatestNews/LatestNews.tsx
@@ -10,12 +10,12 @@ const LatestNews = () => {
         <div className='w-[80%] mx-auto pt-[4rem] grid grid-cols-1 lg:grid-cols-5 gap-[2rem]'>
             {/* Big Card */}
             <div data-aos="fade-right" data-aos-anchor-placement="top-center" className='col-span-3'>
-                <NewsCard image='/n1.jpg' date='0ct 27, 2024' title='Jubilee clever subs wins them 3 points' height='h-[400px]' />
+                <NewsCard image='/n1.jpg' date='0ct 27, 2024' title='Alpha Brace secure Jubilee all 3 points' height='h-[400px]' />
             </div>
             {/* small card */}
             <div className='col-span-2'>
                 <div data-aos="zoom-in" data-aos-anchor-placement="top-center" data-aos-delay="200">
-                    <SmallNewsCard image='/n2.png' date='0ct 27, 2024' title='Arsenal and Liverpool play out a 4 goal thriller draw '/>
+                    <SmallNewsCard image='/n2.png' date='0ct 27, 2024' title='Baleba Red Card  '/>
                 </div>
                 <div data-aos="zoom-in" data-aos-anchor-placement="top-center" data-aos-delay="400" className='mt-[1.4rem] mb-[1.4rem]'>
                     <SmallNewsCard image='/n3.png' date='0ct 26, 2024' title='Barcelona demolish Real Madrid with La Masia graduates at the Bernabou'/>
