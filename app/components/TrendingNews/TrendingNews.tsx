3fa1bfe2b48ef2bb855531afe93a7e166ae39400
IcodeAlpha <alphacode4@outlook.com>
2025-01-01 09:52:10 +0300

    new trending news

--- app/components/TrendingNews/TrendingNews.tsx
+++ app/components/TrendingNews/TrendingNews.tsx
@@ -5,7 +5,7 @@ const TrendingNews = () => {
   return (
     <div className='pt-[5rem] pb-[3rem]'>
         {/* heading */}
-        <h1 className='heading'>Trending News</h1>
+        <h1 className='heading'>New Trends</h1>
         {/* cards div */}
         <div className='pt-[2rem] md:pt-[3rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[2rem]'>
             {/* Card 1 */}
@@ -13,7 +13,7 @@ const TrendingNews = () => {
                 <NewsCard 
                 image="/n1.jpg" 
                 date='Oct 27, 2024' 
-                title='More difficult fixtures on the way for Jubilee?' 
+                title='United Play Liverpool on the new year opener?' 
                 height='h-[300px]'
                 />
             </div>
@@ -22,7 +22,7 @@ const TrendingNews = () => {
                 <NewsCard 
                 image="/n2.png" 
                 date='Oct 27, 2024' 
-                title='Mwau misses from the spot' 
+                title='Arsenal scores from corners again' 
                 height='h-[300px]'
                 />
             </div>
@@ -31,7 +31,7 @@ const TrendingNews = () => {
                 <NewsCard 
                 image="/n3.png" 
                 date='Oct 27, 2024' 
-                title='Oracle held to a 1-1 draw by Kutus at home' 
+                title='Ipswich beat Chelsea' 
                 height='h-[300px]'
                 />
             </div>
@@ -40,7 +40,7 @@ const TrendingNews = () => {
                 <NewsCard 
                 image="/n4.png" 
                 date='Oct 27, 2024' 
-                title='Riakithiga sinks Kiamiciri in injury time' 
+                title='Barcelona unable to register Dani Olmo' 
                 height='h-[300px]'
                 />
             </div>
